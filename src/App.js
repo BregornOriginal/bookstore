ce06be72c4b11305b54b9933b685fe154c22dc65
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-07-07 18:55:57 -0300

    create react app and add linters

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit and save to reload.
+        </p>
+        Learn React
+      </header>
+    </div>
+  );
+}
+
+export default App;
