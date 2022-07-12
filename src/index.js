e216883e313cb8c1bf61dbb66822ba74d40e5ecc
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-07-12 14:44:01 -0300

    add store of redux to the app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
+import { addBook } from './redux/books/books';
+import store from './redux/store/configureStore';
+
+console.log(addBook());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
 );
