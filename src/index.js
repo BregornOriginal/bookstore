bef0d32ffb24e68f6f18b5961533b77c9c9abeda
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-07-12 14:44:26 -0300

    delete console.log code

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
-import { addBook } from './redux/books/books';
 import store from './redux/store/configureStore';
-
-console.log(addBook());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
