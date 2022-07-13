abdc74b367da80a07cd0750d6d5e723bc206318e
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-07-13 19:30:57 -0300

    install axios dependencie and create an app on the api

---
+++ src/base-api.js
@@ -0,0 +1,15 @@
+// import axios from 'axios';
+
+// const baseURL = axios.create({
+//   baseURL: 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi',
+//   headers: {
+//     'Content-type': 'application/json, text/plain, */*',
+//   },
+// });
+
+// this next function was only to create a new app on the API
+// const newApp = () => baseURL.post('/apps/');
+
+const myBookstoreApi = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/jlFnXWgXTgySgXTsONFO';
+
+export default myBookstoreApi;
